76ac6a3af6663b1de24990c4a14ea642664da3db
Kingclove <klouse@outlook.com>
2014-05-21 01:08:47 -0500

    Finished Tutorial

---
+++ src/com/scheme/myrealfirst/DisplayMessageActivity.java
@@ -0,0 +1,50 @@
+package com.scheme.myrealfirst;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	
+	Intent intent = getIntent();
+	
+	String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+	
+	TextView textview = new TextView(this);
+	textview.setTextSize(40);
+	
+	textview.setText(message);
+	
+	setContentView(textview);
+
+	
+    }
+
+    
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+	// Handle action bar item clicks here. The action bar will
+	// automatically handle clicks on the Home/Up button, so long
+	// as you specify a parent activity in AndroidManifest.xml.
+	int id = item.getItemId();
+	if (id == R.id.action_settings) {
+	    return true;
+	}
+	return super.onOptionsItemSelected(item);
+    }
+
+    
+
+}
